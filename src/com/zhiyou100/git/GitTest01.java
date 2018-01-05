a1d9f85fb9887c365f96194cc636743d1ca0c70c
Shmilyls <1092732778@qq.com>
2018-01-05 17:34:59 +0800

    恢复到添加文件

--- src/com/zhiyou100/git/GitTest01.java
+++ src/com/zhiyou100/git/GitTest01.java
@@ -1,15 +1,15 @@
 package com.zhiyou100.git;
 
 /**  
-* @ClassName: GitTest02  
+* @ClassName: GitTest01  
 * @Description: TODO
 * @author Shmily  
-* @date 2018年1月5日  下午5:29:55
+* @date 2018年1月5日  下午5:26:47
 *  
 */
-public class GitTest02 {
+public class GitTest01 {
 
 	public static void main(String[] args) {
-		System.out.println("新增文件");
+		System.out.println("新建项目");
 	}
 }
